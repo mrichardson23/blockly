7a10b9ccb65261e2049f729937748428afdef9a7
Matt Richardson <mrichardson23@gmail.com>
2016-05-25 10:22:44 -0700

    implement code generators

--- generators/python/imaging.js
+++ generators/python/imaging.js
@@ -5,7 +5,12 @@ goog.provide('Blockly.Python.pi-camera');
 goog.require('Blockly.Python');
 
 Blockly.Python['take_photo'] = function(block) {
-  var code = 'import picamera\nwith picamera.PiCamera() as camera:\n\tcamera.capture(\'/tmp/foo.jpg\')';
+  var code = 'import picamera\nimport io\nfromPIL import Image\ncamData = io.BytesIO()\nwith picamera.PiCamera() as camera:\n\tcamera.capture(camData, \'jpeg\')\ncamData.seek(0)\nimage = Image.open(camData)\n';
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['test_photo'] = function(block) {
+  var code = 'from PIL import Image\nimport urllib.request, io\ntestImageData = io.BytesIO(urllib.request.urlopen("http://localhost/test.jpg").read())\nimage = Image.open(testImageData)\n';
   return [code, Blockly.Python.ORDER_ATOMIC];
 };
 
