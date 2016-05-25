e40048c6c3d17a03c386198ffefa9d4be6ccb4a5
Matt Richardson <mrichardson23@gmail.com>
2016-05-25 13:47:29 -0700

    add code window

--- generators/python/imaging.js
+++ generators/python/imaging.js
@@ -6,15 +6,15 @@ goog.require('Blockly.Python');
 
 Blockly.Python['take_photo'] = function(block) {
   var code = 'import picamera\nimport io\nfrom PIL import Image\ncamData = io.BytesIO()\nwith picamera.PiCamera() as camera:\n\tcamera.capture(camData, \'jpeg\')\ncamData.seek(0)\nimage = Image.open(camData)\n';
-  return [code, Blockly.Python.ORDER_ATOMIC];
+  return code;
 };
 
 Blockly.Python['test_photo'] = function(block) {
   var code = 'from PIL import Image\nimport urllib.request, io\ntestImageData = io.BytesIO(urllib.request.urlopen("http://localhost/test.jpg").read())\nimage = Image.open(testImageData)\n';
-  return [code, Blockly.Python.ORDER_ATOMIC];
+  return code;
 };
 
 Blockly.Python['save_to_desktop'] = function(block) {
   var code = 'image.save("/home/pi/Desktop/myPhoto.jpg")\n';
-  return [code, Blockly.Python.ORDER_ATOMIC];
+  return code;
 };
