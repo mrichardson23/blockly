2d235cbcd1bf119d0f682104c7c4121e99ef5467
Matt Richardson <mrichardson23@gmail.com>
2016-05-25 16:20:04 +0000

    add image saver

--- generators/python/imaging.js
+++ generators/python/imaging.js
@@ -7,5 +7,10 @@ goog.require('Blockly.Python');
 
 Blockly.Python['take_photo'] = function(block) {
   var code = 'import picamera\nwith picamera.PiCamera() as camera:\n\tcamera.capture(\'/tmp/foo.jpg\')';
-  return [code, Blockly.Python.ORDER_ADDITION];
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['save_to_desktop'] = function(block) {
+  var code = 'image.save("/home/pi/Desktop/myPhoto.jpg")\n';
+  return [code, Blockly.Python.ORDER_ATOMIC];
 };
